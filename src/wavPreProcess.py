fe87444f967e774548854614083229cde9ac985f
Joseph Moraru <19jam5@queensu.ca>
2024-02-03 12:47:29 -0500

    finished wavPreProcess.py and updated requirements.txt

--- src/wavPreProcess.py
+++ src/wavPreProcess.py
@@ -1,5 +1,6 @@
 import librosa
 import os.path
+import soundfile as sf
 from exceptions import *
 
 '''
@@ -19,9 +20,28 @@ class Resampler():
             print("NotWavError Exception : invalid file type")
         except FileNotFoundError:
             print("FileNotFoundError : Specified file path does not exist")
+        
         self.file = wavFile
+        self.fun16k_counter = 0
+        self.fun8k_counter = 0
 
     def to16k(self):
-        pass
+        self.fun16k_counter += 1
+        new_name = "./src/data/DS_"+self.file[-5]+str(self.fun16k_counter)+".wav"
+        try:
+            audio_signal, sample_rate = librosa.load(self.file, sr=16000)
+            sf.write(file=new_name, data=audio_signal, samplerate=sample_rate)
+        except:
+            print("Something went wrong, cannot downsample to 16k Hz")
 
-res = Resampler('./src/data/stereo_4410.wav')
+    def to8k(self):
+        self.fun8k_counter += 1
+        new_name = "./src/data/DS_"+self.file[-5]+str(self.fun8k_counter)+".wav"
+        try:
+            audio_signal, sample_rate = librosa.load(self.file, sr=8000)
+            sf.write(file=new_name, data=audio_signal, samplerate=sample_rate)
+        except:
+            print("Something went wrong, cannot downsample to 8k Hz")
+
+res = Resampler('./src/data/stereo_44100.wav')
+res.to16k()
