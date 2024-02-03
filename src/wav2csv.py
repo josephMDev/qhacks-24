859879bc0b9d58066c64f46e2aa6236959374330
Joseph Moraru <19jam5@queensu.ca>
2024-02-03 02:28:09 -0500

    added new test stereo wavs

--- src/wav2csv.py
+++ src/wav2csv.py
@@ -2,45 +2,49 @@
 """
 BMCL BAEKSUWHAN
 @author: lukious
+@edited by : josephMDev
 """
-
-import sys, os, os.path
 from scipy.io import wavfile
 import pandas as pd
 
-input_filename = input("Input file number:")
-if input_filename[-3:] != 'wav':
-    print('WARNING!! Input File format should be *.wav')
-    sys.exit()
 
-samrate, data = wavfile.read(str('/qhacks-24/wav-to-csv/wavfile/' + input_filename))
-print('Load is Done! \n')
+def toCSV(inFile : str) :
+    if inFile[-3:] != 'wav':
+        print('WARNING!! Input File format should be *.wav')
+        return False
 
-wavData = pd.DataFrame(data)
+    samrate, data = wavfile.read(str('./src/data/' + inFile))
+    print('Load is Done! \n')
 
-if len(wavData.columns) == 2:
-    print('Stereo .wav file\n')
-    wavData.columns = ['R', 'L']
-    stereo_R = pd.DataFrame(wavData['R'])
-    stereo_L = pd.DataFrame(wavData['L'])
-    print('Saving...\n')
-    stereo_R.to_csv(str(input_filename[:-4] + "_Output_stereo_R.csv"), mode='w')
-    stereo_L.to_csv(str(input_filename[:-4] + "_Output_stereo_L.csv"), mode='w')
-    # wavData.to_csv("Output_stereo_RL.csv", mode='w')
-    print('Save is done ' + str(input_filename[:-4]) + '_Output_stereo_R.csv , '
-                          + str(input_filename[:-4]) + '_Output_stereo_L.csv')
+    wavData = pd.DataFrame(data)
 
-elif len(wavData.columns) == 1:
-    print('Mono .wav file\n')
-    wavData.columns = ['M']
+    if len(wavData.columns) == 2:
+        print('Stereo .wav file\n')
+        wavData.columns = ['R', 'L']
+        stereo_R = pd.DataFrame(wavData['R'])
+        stereo_L = pd.DataFrame(wavData['L'])
+        print('Saving...\n')
+        #stereo_R.to_csv(str(inFile[:-4] + "./src/data/_Output_stereo_R.csv"), mode='w')
+        #stereo_L.to_csv(str(inFile[:-4] + "./src/data/_Output_stereo_L.csv"), mode='w')
+        wavData.to_csv("./src/data/Output_stereo_RL.csv", mode='w')
+        #print('Save is done ' + str(inFile[:-4]) + './src/data/_Output_stereo_R.csv , '
+                            #+ str(inFile[:-4]) + './src/data/_Output_stereo_L.csv')
 
-    wavData.to_csv(str(input_filename[:-4] + "_Output_mono.csv"), mode='w')
+    elif len(wavData.columns) == 1:
+        print('Mono .wav file\n')
+        wavData.columns = ['M']
 
-    print('Save is done ' + str(input_filename[:-4]) + '_Output_mono.csv')
+        wavData.to_csv(str("./src/data/" + inFile[:-4] + "_Output_mono.csv"), mode='w')
 
-else:
-    print('Multi channel .wav file\n')
-    print('number of channel : ' + len(wavData.columns) + '\n')
-    wavData.to_csv(str(input_filename[:-4] + "Output_multi_channel.csv"), mode='w')
+        print('Save is done ' + str("./src/data/" + inFile[:-4] + '_Output_mono.csv'))
 
-    print('Save is done ' + str(input_filename[:-4]) + 'Output_multi_channel.csv')
+    else:
+        print('Multi channel .wav file\n')
+        print('number of channel : ' + len(wavData.columns) + '\n')
+        wavData.to_csv(str("./src/data/" + inFile[:-4] + "Output_multi_channel.csv"), mode='w')
+
+        print('Save is done ' + str("./src/data/" + inFile[:-4] + 'Output_multi_channel.csv'))
+    return True
+
+if __name__ == '__main__':
+    toCSV('stereo_44100.wav')
