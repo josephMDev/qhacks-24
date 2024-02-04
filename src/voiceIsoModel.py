1849e55be3757745e2dbabd95365fa6af1fc4062
Joseph Moraru <19jam5@queensu.ca>
2024-02-03 20:44:22 -0500

    renamed voiceIsoModel.py & implemented callable class

--- src/voiceIsoModel.py
+++ src/voiceIsoModel.py
@@ -2,7 +2,7 @@ from asteroid.models import BaseModel
 import soundfile as sf
 
 # 'from_pretrained' automatically uses the right model class (asteroid.models.DPRNNTasNet).
-model = BaseModel.from_pretrained("mpariente/DPRNNTasNet-ks2_WHAM_sepclean")
+#model = BaseModel.from_pretrained("mpariente/DPRNNTasNet-ks2_WHAM_sepclean")
 '''
 # You can pass a NumPy array:
 mixture, _ = sf.read("female-female-mixture.wav", dtype="float32", always_2d=True)
@@ -12,4 +12,10 @@ mixture = mixture.reshape(1, mixture.shape[0], mixture.shape[1])
 out_wavs = model.separate(mixture)
 '''
 # Or simply a file name:
-model.separate("./src/data/input/mix7.wav",force_overwrite=True,resample=True,output_dir="./src/data/output/")
+class PreTrainedModel():
+    def __init__(self, pretrained_m : str = "mpariente/DPRNNTasNet-ks2_WHAM_sepclean"):
+        self.pretrained_m = pretrained_m
+        self.model = BaseModel.from_pretrained(self.pretrained_m)
+    
+    def sepAudio(self, inFile : str, overwrite : bool = True, rs : bool = True):
+        self.model.separate(inFile,force_overwrite=overwrite,resample=rs,output_dir="./src/data/output/")
